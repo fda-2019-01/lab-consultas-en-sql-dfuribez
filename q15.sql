896421f3d4b391a18b30ccd6bb128143fe740c19
Diego Uribe <dfuribez@unal.eu.co>
2019-08-27 11:02:46 -0500

    Solucionado

--- q15.sql
+++ q15.sql
@@ -5,3 +5,15 @@
 ## a 400.
 ##
 
+SELECT
+    tbl1.k0, avg(tbl2.c21) AS "avg(c21)"
+FROM
+    tbl1
+JOIN
+    tbl2 
+    ON
+        tbl1.k1 = tbl2.k1
+WHERE
+    tbl1.c13 > 400
+GROUP BY
+    tbl1.k0
